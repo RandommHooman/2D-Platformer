241f2e3ff7127e09608cfa8f0bea9e747b52d11e
RandommHooman <158496695+RandommHooman@users.noreply.github.com>
2024-12-06 22:41:08 +0800

    Add files via upload

---
+++ LoadSave.java
@@ -0,0 +1,51 @@
+package utilz;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import platformer.Game;
+
+public class LoadSave {
+
+	public static final String PLAYER_ATLAS = "aethers1.png";
+	public static final String LEVEL_ATLAS = "pixilart-sprite.png";
+	public static final String LEVEL_ONE_DATA = "pixil-frame-0.png";
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+
+	public static int[][] GetLevelData() {
+		int[][] lvlData = new int[platformer.Game.TILES_IN_HEIGHT][platformer.Game.TILES_IN_WIDTH];
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+
+		for (int j = 0; j < img.getHeight(); j++)
+			for (int i = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getRed();
+				if (value >= 48)
+					value = 0;
+				lvlData[j][i] = value;
+			}
+		return lvlData;
+
+	}
+}
---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package platformer;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		new Game();
+
+	}
+
+}
